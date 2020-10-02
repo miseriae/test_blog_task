9524c94b1e4e10c42224d5dc7040f8635f0ab2d1
miseriae <zuyev.frm@gmail.com>
2020-10-02 16:31:26 +0300

    Fix typo

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('arctile/<slug:post>', views.post_detail, name='post_detail'),
+    path('article/<slug:post>', views.post_detail, name='post_detail'),
     path('add_post/', AddPostView.as_view(), name='add_post')
 ]
